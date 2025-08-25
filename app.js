22f6b75063b11e6f0f13ef60671c83b6fca7e127
Risfrhn <rskyfrhn801@gmail.com>
2025-08-25 10:44:24 +0800

    Second Commit
    - Menambahkan file views
    - Melakukan setup dashboard dan pemisahan layouting

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+app.use(express.static("node_modules"));
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('Dashboard', {
+    tittle: 'Dashboard',
+  })
 })
 
 app.use('/', (req, res) => {
