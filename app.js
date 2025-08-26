493a8083be50429ffdb4870b183f98fc5bc0241d
Muhammad Risky Farhan <rskyfrhn801@gmail.com>
2025-08-26 12:10:25 +0700

    Third Commit
    - Menambahkan koneksi database menggunakan mongodb

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+// db
+require('./utils/db');
+const User = require('./models.User');  
+
+
+
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://127.0.0.1:27017";
+const dbName = "Trackers";
+
+const client = new MongoClient(uri); // nggak perlu lagi pakai useNewUrlParser & useUnifiedTopology
+
+
+
+
+
 app.set('view engine', 'ejs');
 app.use(express.static("node_modules"));
 app.use(express.static('public'));
@@ -9,6 +27,11 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('Dashboard', {
     tittle: 'Dashboard',
+  })
+})
+app.get('/Tracker', (req, res) => {
+  res.render('Tracker', {
+    tittle: 'Tracker',
   })
 })
 
@@ -20,3 +43,5 @@ app.use('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
+
+
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Definisikan Schema
+const userSchema = new mongoose.Schema({
+  nama: {
+    type: String,
+    required: true,
+  },
+  username: {   // biasain lowercase biar konsisten
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'worker'], // hanya bisa Admin atau worker
+    required: true,
+  }
+});
+
+// Buat Model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
